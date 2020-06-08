c3baa9d8fff46ed26c4911fb4f99f389897c0226
GihanDarshana <45061133+GihanDarshana@users.noreply.github.com>
2020-06-08 21:41:22 +0530

    First commit

---
+++ Data/EmployeeContext.cs
@@ -0,0 +1,14 @@
+using EmployeeCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCRUD.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
